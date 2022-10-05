84ff346a13e312f6f39d309bfb2a177fcd34c6ca
D4ITO <henryzarlenga12@gmail.com>
2022-10-05 14:57:20 -0300

    font ok

--- devfinances/src/styles/global.ts
+++ devfinances/src/styles/global.ts
@@ -1,8 +1,28 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle `
+export const GlobalStyle = createGlobalStyle`
     :root{
         --background: #F0F1F5;
+
+        --cabecalho: #2D4A22;
+
+        --box: #363F5F;
+
+        --titleSaidas: #969CB2;
+        --titleExpense: red;
+
+        --backgroundTotal: #49AA26;
+
+    }
+    
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 728px) {
+            font-size: 87.5%;
+        }
     }
 
     * {
@@ -14,6 +34,10 @@ export const GlobalStyle = createGlobalStyle `
     body {
         background: let(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
     }
         
 
